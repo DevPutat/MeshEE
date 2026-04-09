bad894bb02a0404fe9ae91fc0ee411d108948356
A.Neumyvalchenko <neumyvalchenkoaa@eastmining.ru>
2026-04-09 22:10:17 +0700

    Add login form and tauri composables

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@ import { onUnmounted, Ref, ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { listen, type UnlistenFn } from "@tauri-apps/api/event";
 import { Message } from "./types";
-import BaseButton from "./domains/main/components/BaseButton.vue";
-import BaseInput from "./domains/main/components/BaseInput.vue";
-import { useTauriWindow } from "./composables/useTauriWindow";
+import { useAuth } from "./composables/useAuth";
+import LoginForm from "./domains/auth/components/LoginForm.vue";
+import BaseTauriAppBar from "./domains/main/components/BaseTauriAppBar.vue";
 
-const {
-  closeWindow,
-  toggleMaximizeWindow,
-  minimizeWindow
-} = useTauriWindow();
+const { user } = useAuth();
 
 const greetMsg = ref("");
 const name = ref("");
+
+
+
+
+
+
+
+
 
 const messages: Ref<Message[]> = ref([]);
 
@@ -56,37 +60,11 @@ async function greet() {
 
 <template>
   <div class="container">
-    <div data-tauri-drag-region class="titlebar">
-      <div class="titlebar-text">MeshEE</div>
-      <div class="titlebar-buttons">
-        <button class="control-btn" @click="minimizeWindow">_</button>
-        <button class="control-btn" @click="toggleMaximizeWindow">□</button>
-        <button class="control-btn" @click="closeWindow">×</button>
-      </div>
-    </div>
+    <BaseTauriAppBar />
     <main class="container pa-2">
-    <form class="row" @submit.prevent="greet">
-      <BaseInput id="greet-input" v-model="name" placeholder="Enter a name..."/>
-      <BaseButton type="submit">Greet</BaseButton>
-    </form>
-    <p>{{ greetMsg }}</p>
-    <div>
-      <BaseButton @click="startReceiving" :disabled="isReceiving">
-        {{ isReceiving ? "Получение..." : "Начать получение сообщений" }}
-      </BaseButton>
-      <div v-if="messages.length">
-        <h3>Сообщения:</h3>
-        <ul>
-          <li v-for="msg in messages" :key="msg.id">
-            {{ msg.msg }}
-          </li>
-        </ul>
-      </div>
-      <p v-else>Нет сообщений</p>
-    </div>
-  </main>
+      <LoginForm v-if="!user" />
+    </main>
   </div>
-
 </template>
 
 <style>
